a27046bf162dd215c7f832dcdabd48a99a39aad8
tienTran42984 <nhtetien@gmail.com>
2026-02-07 09:05:29 +0700

    Commit 7/2/26 9:05AM - Adding/Update Session

--- Instructor/app.js
+++ Instructor/app.js
@@ -89,6 +89,13 @@ async function loadPage(pageName) {
     if(pageName === "sessions"){
         renderSessions();
     }
+    if(pageName === "add_sessions"){
+        setupAddingSession()
+    }
+
+    if(pageName === "edit_sessions"){
+        setupEditSession()
+    }
 }
 
 function setupAccountForm() {
@@ -167,6 +174,7 @@ function renderCourses() {
 function renderSessions() {
     const text = document.getElementById("sessionsForText");
     const sessionsBody = document.getElementById("sessionsBody");
+    const addSessionBtn = document.getElementById("addSessionBtn");
     const course = JSON.parse(localStorage.getItem("selectedCourse"));
     const courseSessions = sessions.filter(s => s.courseId === course.id);
 
@@ -261,4 +269,114 @@ function renderSessions() {
             }
         })
     })
+
+    document.querySelectorAll(".edit-session-btn").forEach(btn => {
+        btn.addEventListener("click", () => {
+            const sessionNo =  Number(btn.dataset.sessionId)
+            const course = JSON.parse(localStorage.getItem("selectedCourse"))
+            const session = sessions.find(s => s.sessionNo === sessionNo && s.courseId === course.id)
+
+            localStorage.setItem("editingSession", JSON.stringify(session))
+            loadPage("edit_sessions")
+        })
+    })
+
+    addSessionBtn.addEventListener("click", () => {
+        loadPage("add_sessions")
+    })
+}
+
+function setupAddingSession() {
+    const form = document.getElementById("addSessionForm")
+    
+    const videoInput = document.getElementById("videoFile")
+    const videoPreview = document.getElementById("videoPreview")
+    const videoEmpty = document.getElementById("videoEmptyText")
+
+    videoInput.addEventListener("change", () => {
+        const file = videoInput.files[0]
+
+        const url = URL.createObjectURL(file)
+        videoPreview.src = url
+        videoPreview.style.display = "block";
+        videoEmpty.style.display = "none";
+    })
+
+    form.addEventListener("submit", (e) => {
+        e.preventDefault()
+
+        const course = JSON.parse(localStorage.getItem("selectedCourse"))
+        const videoFile = document.getElementById("videoFile").files[0]
+        const pdfFile = document.getElementById("pdfFile").files[0]
+
+        if (!videoFile && !pdfFile) {
+            alert("Please upload both MP4 and PDF.");
+            return;
+        }
+
+        const courseSessions = sessions.filter(s => s.courseId === course.id);
+        const nextSessionNo = courseSessions.length > 0 ? Math.max(...courseSessions.map(s => s.sessionNo)) + 1 : 1
+        
+        const newSession = {
+            sessionNo: nextSessionNo,
+            courseId: course.id,
+            videoUrl: URL.createObjectURL(videoFile),
+            pdfUrl: URL.createObjectURL(pdfFile)
+        }
+
+        sessions.push(newSession)
+        alert(`Session ${newSession} added`);
+        form.reset()
+        loadPage("sessions")
+    })
+}
+
+function setupEditSession(){
+    const form = document.getElementById("editSessionForm")
+    const videoPreview = document.getElementById("videoPreview")
+    const videoEmpty = document.getElementById("videoEmptyText")
+    const viewPdfBtn = document.getElementById("previewPdf");
+    const newVideo = document.getElementById("videoFile");
+    const newPdf = document.getElementById("pdfFile")
+
+    const session = JSON.parse(localStorage.getItem("editingSession"))
+    console.log(session)
+    console.log(session.videoUrl);
+
+    if(session.videoUrl){
+        console.log(session.videoUrl);
+        videoPreview.src = session.videoUrl
+        videoPreview.style.display = "block"
+        videoEmpty.style.display = "none"
+    }
+
+    viewPdfBtn.addEventListener("click", () => {
+        window.open(session.pdfUrl, "_blank")
+    })
+
+    newVideo.addEventListener("change", () => {
+        const file = newVideo.files[0]
+
+        const url = URL.createObjectURL(file)
+        videoPreview.src = url
+        videoPreview.style.display = "block";
+        videoEmpty.style.display = "none";
+    })
+
+    form.addEventListener("submit", (e) => {
+        e.preventDefault()
+
+        const videoFile = newVideo.files[0]
+        const pdfFile = newPdf.files[0]
+
+        if (videoFile) session.videoUrl = URL.createObjectURL(videoFile)
+        if (pdfFile) session.pdfUrl = URL.createObjectURL(pdfFile)
+
+        const index = sessions.findIndex(s => s.sessionNo === session.sessionNo)
+        if (index !== -1) sessions[index] = session
+
+        alert("Session updated")
+        loadPage("sessions")
+    })
+
 }
