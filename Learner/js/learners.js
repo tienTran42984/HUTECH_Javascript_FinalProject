55971e55cd9282eeaa73a6d47ab5122a0f8b4b78
tienTran42984 <nhtetien@gmail.com>
2026-02-28 19:23:20 +0700

    Commit 28/2/26 - 7:22PM - FixRegisterAccount/LearnerDemo

---
+++ Learner/js/learners.js
@@ -0,0 +1,10 @@
+const students = [
+    {
+        id: "EMP-001",
+        name: "Huong Le",
+        email: "huongle@gmail.com",
+        accountId: 1
+    },
+]
+
+localStorage.setItem("students", JSON.stringify(students));
